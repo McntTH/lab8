3cff2ecb968679228dbb9860defd08c4685a914f
KanitnanTH <MJU6404101354@mju.ac.th>
2023-02-09 20:39:15 +0700

    เสร็จเเล้ว

--- lab8/src/main.js
+++ lab8/src/main.js
@@ -3,8 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import"bootstrap/dist/css/bootstrap.css";
 import"bootstrap/dist/js/bootstrap.js";
+import"bootstrap/dist/css/bootstrap.css";
 
 //import './assets/main.css'//
 
